0652eccacd3ae91821e20b2fd44b587d040e5dfc
최형우 <able@choehyeong-uui-MacBookPro.local>
2025-02-18 13:43:55 +0900

    fix: podspec, config 롤백

--- react-native.config.js
+++ react-native.config.js
@@ -22,7 +22,7 @@ const project = (() => {
 module.exports = {
   dependencies: {
     // Help rn-cli find and autolink this library
-    'react-native-whatssub-webview': {
+    'react-native-webview': {
       root: __dirname,
     },
   },
